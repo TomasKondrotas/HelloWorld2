8f0fe512ab0aa132578d4ba54151b3f6e224740b
TomasKondrotas <tomaskond123@gmail.com>
2020-09-25 13:09:21 +0300

    Added TextChangeButton

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView TvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.TvMain = findViewById(R.id.TvMain);
+    }
+
+    public void ChangeText(View view) {
+        this.TvMain.setText("Text Was Changed");
     }
 }
